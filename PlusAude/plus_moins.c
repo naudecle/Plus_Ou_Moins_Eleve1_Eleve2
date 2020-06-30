9fbb039edcc29e9c0242d6ba150ac4bd68f2f852
naudecle <cleriaaude@gmail.com>
2020-06-30 16:28:16 +0400

    v0.1

--- PlusAude/plus_moins.c
+++ PlusAude/plus_moins.c
@@ -3,18 +3,28 @@
 #include <time.h>
 #include <unistd.h>
 
-void plus_moins(int choix_joueuer, int valeur_a_trouver)
+void plus_moins(int choix_joueur, int valeur_a_trouver, int choix)
 {
-	if (choix_joueuer < valeur_a_trouver)
+	int i=0;
+
+	do
 	{
-		printf("plus \n");
-	}
-	else if (choix_joueuer > valeur_a_trouver)
-	{
-		printf("moins \n");
-	}
-	else
-		printf("Bingo \n");
+		scanf("%d", &choix_joueur);
+		if (choix_joueur < valeur_a_trouver)
+		{
+			printf("plus \n");
+
+		}
+		else if (choix_joueur > valeur_a_trouver)
+		{
+			printf("moins \n");
+		}
+		else
+			printf("Bingo \n");
+		i++;
+
+	}while(choix_joueur != valeur_a_trouver && i < choix);
+	
 }
 int main()
 {
@@ -22,8 +32,8 @@ int main()
 	srand(time(NULL));
 	int n=rand()%100;
 	int v;
+	int p=5;
 	printf("Devienez un entier: ");
-	scanf("%d", &v);
-	plus_moins(v,n);
+	plus_moins(v,n,p);
 	return 0;
 }
